de11cdb2825cb6114b59cac197b00c96d207e674
Thomas Kraemer <Thomas.kraemer@bmw.de>
2019-04-03 16:32:14 +0200

    HttpWindowsTest: Restructured WebSitePlaceholder

--- HttpWindowsTest/WebSite.h
+++ HttpWindowsTest/WebSite.h
@@ -1,66 +1,7 @@
 #pragma once
-
+#include "WebSitePlaceholders.h"
 #include <set>
-#include <vector>
-#include <map>
 using namespace std;
-
-class WebRequest;
-class WebSitePlaceholderGroup;
-
-class WebSitePlaceholder
-{
-	friend class WebSite;
-	friend class WebSitePlaceholderGroup;
-
-protected:
-	void log(string str) { printf((str + "\n").c_str()); }
-	void verbose(string str) { printf((str + "\n").c_str()); }
-	virtual WebSitePlaceholderGroup* asGroup() { return NULL; }
-
-public:
-	string name;
-	string value;
-	string preceding;
-	virtual string generate();
-
-	virtual WebSitePlaceholder* clone();
-
-	WebSitePlaceholder();
-	~WebSitePlaceholder();
-	virtual bool isGroup() { return false; }
-	virtual void prepare();
-};
-
-class WebSitePlaceholderGroup : public WebSitePlaceholder
-{
-	bool isDummy() { return parentGroup != NULL && parentGroup->name != name; }
-	friend class WebSite;
-	WebSitePlaceholder* addValueDefinition(string name, string preceding);
-	WebSitePlaceholderGroup* addGroupDefinition(string name, string preceding);
-	WebSitePlaceholderGroup* parentGroup;
-	void clearValues();
-	virtual WebSitePlaceholder* clone();
-	void cloneInternal(WebSitePlaceholderGroup* clone);
-	bool insertGroup(WebSitePlaceholderGroup& group);
-
-protected:
-	virtual WebSitePlaceholderGroup* asGroup() { return this; }
-
-public:
-	WebSitePlaceholderGroup();
-	~WebSitePlaceholderGroup();
-	vector<WebSitePlaceholder*> childrenDefinitions;
-	vector<WebSitePlaceholder*> childrenValues;
-	virtual string generate();
-
-	WebSitePlaceholderGroup* addGroup();
-	virtual bool isGroup() { return true; }
-
-	void setValues(const map<string, string>& values);
-	void setGroup(const string& groupname, const map<string, string>& values);
-	virtual void prepare();
-};
 
 class WebSite
 {
